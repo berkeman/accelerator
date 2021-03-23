50bef6e3b99779be994d757b5a4a98670a535e0b
Anders Berkeman <anders.berkeman@ericsson.com>
2021-03-23 16:47:38 +0000

    [script] ok?

--- accelerator/shell/script.py
+++ accelerator/shell/script.py
@@ -28,12 +28,21 @@ from collections import defaultdict, OrderedDict
 from accelerator.compat import terminal_size
 from accelerator.shell.parser import printdesc
 
+# ax script                    - lista alla packages + methods
+# ax script build              - lista alla "build.py" i alla packages
+# ax script build_tests        - hitta och visa ett specifikt script (i package-prioordning)
+# ax script foo                - hitta och visa alla build_foo.py i all packages
+# ax script dev                - lista alla script i ett givet package
+# ax script dev.               - "
+# ax script dev.build          - visa dev.build.py
+# ax script testo.foo          - visa testo.build_foo.py
+
 def main(argv, cfg):
 	prog = argv.pop(0)
 	if '--help' in argv or '-h' in argv:
 		print('usage: %s [method]' % (prog,))
-		print('gives description and options for method,')
-		print('or lists methods with no method specified.')
+		print('gives description for build script,')
+		print('or lists build scrips with no description.')
 		return
 	columns = terminal_size().columns
 
@@ -51,57 +60,32 @@ def main(argv, cfg):
 		allscripts[package] = scripts
 
 	if argv:
-		# ax script                    - lista alla packages + methods
-        # ax script build              - lista alla "build.py" i alla packages
-		# ax script build_tests        - hitta och visa ett specifikt script (i package-prioordning)
-		# ax script foo                - hitta och visa alla build_foo.py i all packages
-		# ax script dev                - lista alla script i ett givet package
-		# ax script dev.               - "
-		# ax script dev.build          - visa dev.build.py
-		# ax script testo.foo          - visa testo.build_foo.py
 		for arg in argv:
 			arg = arg.rstrip('.')
 			if arg in allscripts:
-				# arg is matching a package
+				print('%s:' % (arg,))
 				name2desc = allscripts[arg]
-				print('%s:' % (package,))
 				for name, description in name2desc.items():
-					printdesc(name, description or '-', columns)
-				# proceed with next arg
-				continue
-			# Now we know that the argument is not a package.
-			# Then, it can be "name" or "package.name" or complete bogus
-#			print('# arg', arg)
-			if '.' in arg:
+					printdesc(name, description, columns)
+			elif '.' in arg:
 				argpack, argname = arg.rsplit('.', 1)
 				if argpack in allscripts:
-#					print('# dot', argpack, argname)
 					if not argname.startswith('build'):
 						argname = 'build_' + argname
-#					print('# dot', argpack, argname)
 					description = allscripts[argpack].get(argname)
-#					print('# desc', description)
 					if description is not None:
-						description = description or '-'
-						printdesc(argpack + '.' + argname, description or '-', columns)
-						continue
+						printdesc(argpack + '.' + argname, description, columns)
 			else:
 				for package in allscripts:
-					argpack, argname = False, arg
-#					print('# nodot', argpack, argname)
-					if not argname.startswith('build'):
-						argname = 'build_' + argname
-#					print('#', package, argname)
-#					print('#', allscripts[package])
-					description = allscripts[package].get(argname)
+					if not arg.startswith('build'):
+						arg = 'build_' + arg
+					description = allscripts[package].get(arg)
 					if description is not None:
-						description = description or '-'
-						printdesc(package + '.' + argname, description or '-', columns)
+						printdesc(package + '.' + arg, description, columns)
 						continue
 	else:
 		for package, name2desc in allscripts.items():
 			print('%s:' % (package,))
 			for name, description in name2desc.items():
 				if description is not None:
-					description = description or '-'
-				printdesc(name, description, columns)
+					printdesc(name, description, columns)
